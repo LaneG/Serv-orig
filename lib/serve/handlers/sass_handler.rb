d42cdee6f8dd2e690dec4a130c6594f539b39278
Lane Halley <lane@carbonfive.com>
2012-08-22 20:02:09 -0700

    original commit

---
+++ lib/serve/handlers/sass_handler.rb
@@ -0,0 +1,32 @@
+module Serve #:nodoc:
+  
+  # TODO: Figure out how to remove the Sass Handler in favor of Tilt
+  # The Sass handler seems to be necessary to keep Tilt from applying a layout
+  # to Sass files. Any one know how to turn this Tilt feature off?
+  
+  class SassHandler < FileTypeHandler #:nodoc:
+    extension 'sass', 'scss'
+    
+    def parse(string, context)
+      require 'sass'
+      engine = Sass::Engine.new(string,
+        :load_paths => [@root_path],
+        :style => :expanded,
+        :syntax => syntax
+      )
+      engine.render
+    end
+    
+    def syntax
+      if extension == 'scss'
+        :scss
+      else
+        :sass
+      end
+    end
+    
+    def content_type
+      'text/css'
+    end
+  end
+end
